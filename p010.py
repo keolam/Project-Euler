0c0a78789f941d4d0e2d205e4190cdd3c7bb6206
Keola Morse <keolamorse@gmail.com>
2019-06-21 17:42:59 -0700

    tenth problem

---
+++ p010.py
@@ -0,0 +1,18 @@
+"""
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+
+"""
+
+from sympy.ntheory import isprime
+
+def sum_of_primes(limit):
+  nums = list(range(1, limit))
+  result = list(filter(lambda x: isprime(x), nums))
+  
+  return sum(result)
+
+print(sum_of_primes(2000000))
+
+#prints: 142913828922
