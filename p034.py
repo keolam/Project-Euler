39a33c7e29bb2952aabb49afd551f8f4aab74014
Keola Morse <keolamorse@gmail.com>
2019-08-07 18:56:57 -0700

    thirty-fourth problem

--- p034.py
+++ p034.py
@@ -7,21 +7,32 @@ Find the sum of all numbers which are equal to the sum of the factorial of their
 Note: as 1! = 1 and 2! = 2 are not sums they are not included.
 '''
 
-
 import math, sys
 
-def compute():
-  ans = sum(i for i in range(3, 10000000) if i == factorial_digit_sum(i))
-  return str(ans)
+def curious_number_sum():
+    ans = sum(i for i in range(3, 10000000) if i == factorial_digit_sum(i))
+    return str(ans)
 
-  def factorial_digit_sum(n):
+def factorial_digit_sum(n):
     result = 0
     while n >= 10000:
-      result += FACTORIAL_DIGITS_SUM_WITH_LEADING_ZEROS[n % 10000]
-      n //= 10000
-    return result + FACTORIAL_DIGITS_SUM_WITHOUT_LEADING_ZEROS[n]
+        result += fact_digit_sum_leading_zeros[n % 10000]
+        n //= 10000
+        return result + fact_digit_sum[n]
 
-FACTORIAL_DIGITS_SUM_WITHOUT_LEADING_ZEROS = [sum(math.factorial(int(c)) for c in str(i)) for i in range(10000)]
-FACTORIAL_DIGITS_SUM_WITH_LEADING_ZEROS = [sum(math.factorial(int(c)) for c in str(i).zfill(4)) for i in range(10000)]
+fact_digit_sum = [sum(math.factorial(int(c)) for c in str(i)) for i in range(10000)]
+fact_digit_sum_leading_zeros = [sum(math.factorial(int(c)) for c in str(i).zfill(4)) for i in range(10000)]
 
-print(compute())
+print(curious_number_sum())
+
+
+#prints: 40730
+
+
+
+
+
+
+
+
+
